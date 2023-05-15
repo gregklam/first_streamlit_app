d2cec4b1c581fd64e67c3e240acf60ceba5a1860
gregklam <gregklam@gmail.com>
2023-05-15 14:30:27 -0700

    Update streamlit_app.py
    deleted line that just showed json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #this just writes all the data to the screen
 
 #take the json version of the response and normalize it using the pandas function json_normalize
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
